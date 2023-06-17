2b8da9ed42f52210247168a5152cbbe4ef82d175
Alex Komoroske <jkomoros@gmail.com>
2023-06-17 14:33:54 -0700

    Move to types and zod objects coexisting

--- src/types.ts
+++ src/types.ts
@@ -6,17 +6,23 @@ export const environment = z.map(z.string(), z.string());
 
 export type Environment = z.infer<typeof environment>;
 
-export const SeedDataPrompt = z.object({
+export const seedDataPrompt = z.object({
     type: z.literal('prompt'),
     prompt: z.string()
 });
 
-export const SeedData = z.discriminatedUnion("type", [
-    SeedDataPrompt
+export type SeedDataPrompt = z.infer<typeof seedDataPrompt>;
+
+export const seedData = z.discriminatedUnion("type", [
+    seedDataPrompt
 ]);
 
-export const SeedPacket = z.object({
+export type SeedData = z.infer<typeof seedData>;
+
+export const seedPacket = z.object({
     version: z.number().int().finite().safe(),
-    seeds: z.map(z.string(), SeedData)
+    seeds: z.map(z.string(), seedData)
 });
+
+export type SeedPacket = z.infer<typeof seedPacket>;
 

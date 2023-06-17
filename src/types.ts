aab504773438e734c709ec31d538f38d5e897f42
Alex Komoroske <jkomoros@gmail.com>
2023-06-17 15:00:52 -0700

    Added a TODO

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import {
     z
 } from 'zod';
 
+//TODO: this should be an object with unknown keys, not a map.
 const environment = z.map(z.string(), z.string());
 
 export type Environment = z.infer<typeof environment>;

dae227221f56952ae2eddaf79e9ee25f0d684a64
Alex Komoroske <jkomoros@gmail.com>
2023-06-17 14:35:10 -0700

    Unexport things that aren't needed yet from types.

--- src/types.ts
+++ src/types.ts
@@ -2,27 +2,27 @@ import {
     z
 } from 'zod';
 
-export const environment = z.map(z.string(), z.string());
+const environment = z.map(z.string(), z.string());
 
 export type Environment = z.infer<typeof environment>;
 
-export const seedDataPrompt = z.object({
+const seedDataPrompt = z.object({
     type: z.literal('prompt'),
     prompt: z.string()
 });
 
-export type SeedDataPrompt = z.infer<typeof seedDataPrompt>;
+type SeedDataPrompt = z.infer<typeof seedDataPrompt>;
 
-export const seedData = z.discriminatedUnion("type", [
+const seedData = z.discriminatedUnion("type", [
     seedDataPrompt
 ]);
 
-export type SeedData = z.infer<typeof seedData>;
+type SeedData = z.infer<typeof seedData>;
 
-export const seedPacket = z.object({
+const seedPacket = z.object({
     version: z.number().int().finite().safe(),
     seeds: z.map(z.string(), seedData)
 });
 
-export type SeedPacket = z.infer<typeof seedPacket>;
+type SeedPacket = z.infer<typeof seedPacket>;
 

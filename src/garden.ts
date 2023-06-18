76ac06df81e01380ca329b52d9e7eb81ae7e16b1
Alex Komoroske <jkomoros@gmail.com>
2023-06-18 10:51:08 -0700

    Garden keeps track of the location of the first planted seed packet.
    Part of #3.

--- src/garden.ts
+++ src/garden.ts
@@ -40,6 +40,7 @@ class Seed {
 export class Garden {
     _env : Environment
     _seeds : {[id : SeedReference] : Seed}
+    _location? : SeedPacketLocation
 
     constructor(environment : EnvironmentData) {
         this._env = new Environment(environment);
@@ -48,6 +49,11 @@ export class Garden {
 
     get environment() : Environment {
         return this._env;
+    }
+
+    //Returns the location of the first seed packet loaded.
+    get location() : SeedPacketLocation | undefined {
+        return this._location;
     }
 
     seed(id : SeedReference = '') : Seed {
@@ -59,6 +65,7 @@ export class Garden {
     }
 
     plantSeedPacket(location: SeedPacketLocation, packet: SeedPacket) {
+        if (!this._location) this._location = location;
         //TODO: combine IDs with the URL they came from so no collisions
         for (const [id, seed] of Object.entries(packet.seeds)) {
             this.plantSeed(id, seed);

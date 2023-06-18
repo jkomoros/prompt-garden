a0c5da7659162b9808b4d9ca4a3ccaadd0342eac
Alex Komoroske <jkomoros@gmail.com>
2023-06-18 15:03:53 -0700

    Factor out a loadLocalGarden in tools/main.ts
    Part of #12.

--- tools/main.ts
+++ tools/main.ts
@@ -12,7 +12,7 @@ import * as path from 'path';
 const ENVIRONMENT_PATH = 'environment.SECRET.json';
 const SEEDS_DIRECTORY = 'seeds/';
 
-const main = async () => {
+export const loadLocalGarden = async() : Promise<Garden> => {
     const data = fs.readFileSync(ENVIRONMENT_PATH).toString();
     const env = environmentData.parse(JSON.parse(data));
     const garden = new Garden(env);
@@ -24,6 +24,11 @@ const main = async () => {
         //TODO: typecheck. Also, why does this pass typechecking?
         garden.plantSeedPacket(filePath, json);
     }
+    return garden;
+}
+
+const main = async () => {
+    const garden = await loadLocalGarden();
     //Select default seed
     const seed = garden.seed();
     const result = await seed.grow();

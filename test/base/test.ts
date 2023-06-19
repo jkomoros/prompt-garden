cbf41ef1a70df990446af66982296ce95904386e
Alex Komoroske <jkomoros@gmail.com>
2023-06-19 13:02:52 -0700

    Switch to not using loadLocalGarden in tests.
    This means that `seeds/default.json` doesn't have to be continually polluted with stuff.
    
    Part of #12.

--- test/base/test.ts
+++ test/base/test.ts
@@ -4,14 +4,35 @@ import {
 } from '../../src/garden.js';
 
 import {
-    loadLocalGarden
-} from '../../tools/util.js';
+    EnvironmentData,
+    seedPacket
+} from '../../src/types.js';
 
 import {
     mockedResult
 } from '../../src/util.js';
 
 import assert from 'assert';
+
+import {
+    readFileSync
+} from 'fs';
+
+const TEST_PACKET_LOCATION = 'test/base/test.json';
+
+const loadTestGarden = () : Garden => {
+    const env : EnvironmentData = {
+        completion_model: 'openai.com:gpt-3.5-turbo',
+        openai_api_key: 'mock_key',
+        mock: true
+    };
+    const garden = new Garden(env);
+    const data = readFileSync(TEST_PACKET_LOCATION).toString();
+    const json = JSON.parse(data);
+    const packet = seedPacket.parse(json);
+    garden.plantSeedPacket(TEST_PACKET_LOCATION, packet);
+    return garden;
+}
 
 describe('Garden smoke test', () => {
     it('basic running', async () => {
@@ -20,12 +41,12 @@ describe('Garden smoke test', () => {
     });
 
     it('handles loading default garden', async () => {
-        const garden = loadLocalGarden();
+        const garden = loadTestGarden();
         assert.notEqual(garden, undefined);
     })
 
     it('prompt respects mock parameter', async () => {
-        const garden = loadLocalGarden({mock: true});
+        const garden = loadTestGarden();
         const seed = garden.seed();
          //Checking type and throwing narrows type
         if (seed.data.type != 'prompt') throw new Error('Unexpected type');
@@ -36,7 +57,7 @@ describe('Garden smoke test', () => {
     })
 
     it('handles echo', async () => {
-        const garden = loadLocalGarden();
+        const garden = loadTestGarden();
         const seed = garden.seed('hello-world');
         //Checking type and throwing narrows type
         if (seed.data.type != 'echo') throw new Error('Unexpected type');
@@ -46,14 +67,14 @@ describe('Garden smoke test', () => {
     })
 
     it('throws for unknown seed', async () => {
-        const garden = loadLocalGarden();
+        const garden = loadTestGarden();
         assert.throws(() => {
             garden.seed('unknown-123');
         })
     })
 
     it('handles a nested seed', async () => {
-        const garden = loadLocalGarden({mock: true});
+        const garden = loadTestGarden();
         const seed = garden.seed('composed-prompt');
         const hellowWorldSeed = garden.seed('hello-world');
         if (hellowWorldSeed.data.type != 'echo') throw new Error('Unexpected type');
@@ -62,7 +83,5 @@ describe('Garden smoke test', () => {
         const golden = mockedResult(hellowWorldSeed.data.message);
         assert.deepEqual(result, golden);
     })
-
-    //TODO: don't have the test seeds be in seeds/default.json since they're weird
 
 });

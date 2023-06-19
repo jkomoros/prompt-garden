deaf051817a8323729beae814f0fae72ee8f32fc
Alex Komoroske <jkomoros@gmail.com>
2023-06-19 10:21:10 -0700

    Add a description to prompt field.
    Part of #7.

--- src/seed_types.ts
+++ src/seed_types.ts
@@ -4,7 +4,7 @@ import {
 
 export const seedDataPrompt = z.object({
     type: z.literal('prompt'),
-    prompt: z.string()
+    prompt: z.string().describe('The full prompt to be passed to the configured commpletion_model')
 });
 
 export type SeedDataPrompt = z.infer<typeof seedDataPrompt>;

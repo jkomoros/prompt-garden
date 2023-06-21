4e72ac1990282e673f895d3c98fa3224866a7c85
Alex Komoroske <jkomoros@gmail.com>
2023-06-20 20:09:49 -0600

    Define packSeedReferenceID
    Part of #3.

--- src/reference.ts
+++ src/reference.ts
@@ -1,10 +1,15 @@
 import {
+	LocalSeedID,
 	SeedPacketLocation,
 	SeedReferenceID,
 	UnpackedSeedReferenceID
 } from './types.js';
 
 const SEED_ID_DELIMITER = '#';
+
+export const packSeedReferenceID = (location: SeedPacketLocation, id : LocalSeedID) : SeedReferenceID => {
+	return location + SEED_ID_DELIMITER + id;
+};
 
 export const unpackSeedReferenceID = (ref : SeedReferenceID, defaultLocation : SeedPacketLocation) : UnpackedSeedReferenceID => {
 	const pieces = ref.split(SEED_ID_DELIMITER);

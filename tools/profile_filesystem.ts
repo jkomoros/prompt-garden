6e88b8a38a6bf00309494302e80c1c5d3fe93f2f
Alex Komoroske <jkomoros@gmail.com>
2023-07-02 14:55:41 -0700

    When logging in local filesystem without `mock`, outputs .log() statements to a log file.
    Part of #14.

--- tools/profile_filesystem.ts
+++ tools/profile_filesystem.ts
@@ -6,12 +6,17 @@ import {
 	LeafValue
 } from '../src/types.js';
 
-import * as fs from 'fs';
+import fs from 'fs';
+import path from 'path';
 
 import inquirer from 'inquirer';
 
+const PROFILE_DIRECTORY = '.profiles/';
+
+const LOG_FILE = 'console.log';
 
 export class ProfileFilesystem extends Profile {
+
 	override async localFetch(location : string) : Promise<unknown> {
 		const file = fs.readFileSync(location).toString();
 		return JSON.parse(file);
@@ -25,5 +30,38 @@ export class ProfileFilesystem extends Profile {
 			default: defaultValue
 		}]);
 		return answers.question;
+	}
+
+	get _profileDir() : string {
+		const garden = this.garden;
+		if (!garden) throw new Error('Garden not yet set');
+		const profile = garden.environment.getKnownSecretKey('profile');
+		if (!profile) throw new Error('Profile not set');
+		return path.join(PROFILE_DIRECTORY, profile);
+	}
+
+	_ensureProfileDir() : void {
+		if (!fs.existsSync(PROFILE_DIRECTORY)) {
+			fs.mkdirSync(PROFILE_DIRECTORY);
+		}
+		const profilePath = this._profileDir;
+		if (!fs.existsSync(profilePath)) {
+			fs.mkdirSync(profilePath);
+		}
+	}
+
+	override log(message?: unknown, ...optionalParams: unknown[]): void {
+		const garden = this.garden;
+		if (garden) {
+			const mock = garden.environment.getKnownBooleanKey('mock');
+			if (!mock) {
+				this._ensureProfileDir();
+				const logFile = path.join(this._profileDir, LOG_FILE);
+				//TODO: better output style (e.g. timestamps, maybe JSON-LD),
+				//and don't drop optionalParams.
+				fs.appendFileSync(logFile, message + '\n');
+			}
+		}
+		super.log(message, ...optionalParams);
 	}
 }

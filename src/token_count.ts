638b57de14d79ea110c3c14eee4f53340101e972
Alex Komoroske <jkomoros@gmail.com>
2023-07-04 05:50:09 -0700

    Make countTokens accept an EmbeddingModelID or a CompletionModelID.
    Part of #26.

--- src/token_count.ts
+++ src/token_count.ts
@@ -5,14 +5,27 @@ import {
 	modelProvider
 } from './types.js';
 
+import {
+	assertUnreachable
+} from './util.js';
+
 export const extractModel = (model : EmbeddingModelID | CompletionModelID) : [name : ModelProvider, modelName : string] => {
 	const parts = model.split(':');
 	if (parts.length != 2) throw new Error('Model didn\'t have : as expected');
 	return [modelProvider.parse(parts[0]), parts[1]];
 };
 
-export const countTokens = async (model : EmbeddingModelID, text : string) : Promise<number> => {
-	if (model != 'openai.com:text-embedding-ada-002') throw new Error(`Unsupported embedding model: ${model}`);
+export const countTokens = async (model : EmbeddingModelID | CompletionModelID, text : string) : Promise<number> => {
+	const [provider] = extractModel(model);
+	
+	//Check to make sure it's a known model in a way that will warn when we add new models.
+	switch(provider) {
+	case 'openai.com':
+		//OK
+		break;
+	default:
+		assertUnreachable(provider);
+	}
 
 	//Note: the types are declared in src/gpt-tok.d.ts, which is set to not be visible in VSCode.
 	const {default: module } = await import('gpt-tok');

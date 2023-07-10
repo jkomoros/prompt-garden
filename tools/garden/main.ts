db6e96e259f77aad6c773ad8d57afda2cbc44751
Alex Komoroske <jkomoros@gmail.com>
2023-07-09 18:43:27 -0700

    Refactor seed growing out of main, since some commands won't end up using it.
    Part of #1.

--- tools/garden/main.ts
+++ tools/garden/main.ts
@@ -6,6 +6,7 @@ import {
 
 import {
 	EnvironmentData,
+	SeedID,
 	seedID
 } from '../../src/types.js';
 
@@ -28,6 +29,7 @@ import {
 import fs from 'fs';
 
 import inquirer from 'inquirer';
+import { Garden } from '../../src/garden.js';
 
 const cliOptions = z.object({
 	seed: z.optional(seedID),
@@ -42,6 +44,34 @@ const cliOptions = z.object({
 });
 
 type CLIOptions = z.infer<typeof cliOptions>;
+
+const growSeed = async (garden : Garden, seedID : SeedID) : Promise<void> => {
+	const options = garden.optionsForID(seedID);
+	if (options.length == 0) {
+		throw new Error(`Unknown seed: ${seedID}`);
+	}
+	let seedRef = options[0];
+	if (options.length > 1) {
+		const answers = await inquirer.prompt([{
+			name: 'question',
+			type: 'list',
+			choices: options.map(option => ({
+				name: packSeedReference(option),
+				value: option
+			})),
+			message: `There are multiple seeds with ID ${seedID}. Which one do you want to use?`,
+			default: options[0]
+		}]);
+		seedRef = answers.question;
+	}
+	//Select default seed
+	const seed = await garden.seed(seedRef);
+	if (!seed) {
+		throw new Error(`Unknown seed "${seedID}"`);
+	}
+	const result = await seed.grow();
+	console.log(result);
+};
 
 const main = async (opts : CLIOptions) => {
 	const overrides : EnvironmentData = {
@@ -72,32 +102,7 @@ ${diagram}
 		exit(0);
 	}
 	const seedID = opts.seed || '';
-	const options = garden.optionsForID(seedID);
-	if (options.length == 0) {
-		throw new Error(`Unknown seed: ${seedID}`);
-	}
-	let seedRef = options[0];
-	if (options.length > 1) {
-		const answers = await inquirer.prompt([{
-			name: 'question',
-			type: 'list',
-			choices: options.map(option => ({
-				name: packSeedReference(option),
-				value: option
-			})),
-			message: `There are multiple seeds with ID ${seedID}. Which one do you want to use?`,
-			default: options[0]
-		}]);
-		seedRef = answers.question;
-	}
-	//Select default seed
-	const seed = await garden.seed(seedRef);
-	if (!seed) {
-		console.error('Unknown seed "' + seedID + '"');
-		exit(1);
-	}
-	const result = await seed.grow();
-	console.log(result);
+	await growSeed(garden, seedID);
 };
 
 (async () => {

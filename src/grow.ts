jkomoros/prompt-garden

commit e4a4e6b1a94b73f6cf14ceebf0abeb93e69b0e82

Add seed_type `join`.
Part of #35.

src/grow.ts
@@ -52,7 +52,8 @@ import {
 	SeedDataRandom,
 	SeedDataRandomSeed,
 	roundType,
-	SeedDataSplit
+	SeedDataSplit,
+	SeedDataJoin
 } from './types.js';
 
 import {
@@ -587,6 +588,14 @@ const growSplit = async (seed : Seed<SeedDataSplit>, env : Environment) : Promis
 	return input.split(delimiter);
 };
 
+const growJoin = async (seed : Seed<SeedDataJoin>, env : Environment) : Promise<string> => {
+	const data = seed.data;
+	const items = extractString(await getProperty(seed, env, data.items, null));
+	if (!Array.isArray(items)) throw new Error('Split expects an array');
+	const delimiter = extractString(await getProperty(seed, env, data.delimiter, ''));
+	return items.join(delimiter);
+};
+
 const growThrow = async (seed : Seed<SeedDataThrow>, env : Environment) : Promise<Value> => {
 	const data = seed.data;
 	const error = extractString(await getProperty(seed, env, data.error, 'Unknown error'));
@@ -800,6 +809,9 @@ export const grow = async (seed : Seed, env : Environment) : Promise<Value> => {
 		break;
 	case 'split':
 		result = await growSplit(seed as Seed<SeedDataSplit>, env);
+		break;
+	case 'join':
+		result = await growJoin(seed as Seed<SeedDataJoin>, env);
 		break;
 	case 'throw':
 		result = await growThrow(seed as Seed<SeedDataThrow>, env);

src/types.ts
@@ -779,6 +779,19 @@ const seedDataSplit = makeSeedData(seedDataConfigSplit);
 
 export type SeedDataSplit = z.infer<typeof seedDataSplit>;
 
+const seedDataConfigJoin = {
+	type: z.literal('join'),
+	properties: {
+		items: inputValueArray.describe('The array to join'),
+		delimiter: z.string().optional().describe('The delimiter to use')
+	}
+};
+
+const nestedSeedDataJoin = makeNestedSeedData(seedDataConfigJoin);
+const seedDataJoin = makeSeedData(seedDataConfigJoin);
+
+export type SeedDataJoin = z.infer<typeof seedDataJoin>;
+
 const seedDataConfigThrow = {
 	type: z.literal('throw'),
 	properties: {
@@ -946,6 +959,7 @@ export const expandedSeedData = z.discriminatedUnion('type', [
 	seedDataArray,
 	seedDataMap,
 	seedDataSplit,
+	seedDataJoin,
 	seedDataThrow,
 	seedDataVar,
 	seedDataRandom,
@@ -990,6 +1004,7 @@ export const seedData = z.discriminatedUnion('type', [
 	nestedSeedDataArray,
 	nestedSeedDataMap,
 	nestedSeedDataSplit,
+	nestedSeedDataJoin,
 	nestedSeedDataThrow,
 	nestedSeedDataVar,
 	nestedSeedDataRandom,

test/base/test.ts
@@ -896,6 +896,14 @@ Suffix`;
 		assert.deepStrictEqual(actual, golden);
 	});
 
+	it('join test', async () => {
+		const garden = loadTestGarden();
+		const seed = await garden.seed('join-test');
+		const actual = await seed.grow();
+		const golden = 'one:two';
+		assert.deepStrictEqual(actual, golden);
+	});
+
 });
 
 describe('expandSeedPacket tests', () => {

commit 08c77578d4cee13fce5566226834d7045974131a

Fixed bug where join didn't work.
Part of #35.

Broken in e4a4e6b1a94b73f6cf14ceebf0abeb93e69b0e82.

src/grow.ts
@@ -544,7 +544,7 @@ const growSplit = async (seed : Seed<SeedDataSplit>, env : Environment) : Promis
 
 const growJoin = async (seed : Seed<SeedDataJoin>, env : Environment) : Promise<string> => {
 	const data = seed.data;
-	const items = extractString(await getProperty(seed, env, data.items, null));
+	const items = await getProperty(seed, env, data.items, null);
 	if (!Array.isArray(items)) throw new Error('Split expects an array');
 	const delimiter = extractString(await getProperty(seed, env, data.delimiter, ''));
 	return items.join(delimiter);
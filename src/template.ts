030d6be01425c485c9dcc53912bf0d5ff7ac2d4f
Alex Komoroske <jkomoros@gmail.com>
2023-07-16 15:28:41 -0700

    Fix a longstanding bug where template type of int would only match a single int.
    Part of #17.

--- src/template.ts
+++ src/template.ts
@@ -57,7 +57,7 @@ const VALUE_CONVERTERS : {[t in TemplateVarType]: (input: string) => (number | s
 
 const VALUE_PATTERNS : {[t in TemplateVarType]: string} = {
 	'string': '.*?',
-	'int': '-?\\d',
+	'int': '-?\\d+',
 	'float': '-?\\d+(\\.\\d+)?',
 	'boolean': [...Object.keys(TRUE_LITERALS), ...Object.keys(FALSE_LITERALS)].join('|')
 };

f0a7aaa261933dac4cf53f61c4318a1ac35fc04d
Alex Komoroske <jkomoros@gmail.com>
2023-07-22 08:01:36 -0700

    Add a getObjectProperty util function.
    It knows how to do dotted property gets.
    
    Part of #50.

--- src/util.ts
+++ src/util.ts
@@ -11,3 +11,16 @@ const ILLEGAL_NAME_CHARS = /[^a-zA-Z0-9_-]/g;
 export const safeName = (input : string) : string => {
 	return input.replace(ILLEGAL_NAME_CHARS, '_');
 };
+
+export const getObjectProperty = (obj : {[name : string]: unknown}, path : string) : unknown => {
+	if (!obj || typeof obj != 'object') throw new Error(`obj must be an object but had the value: ${obj}`);
+	const parts = path.split('.');
+	//if obj is an array then a get of a key like `1` will work as expected
+	//thanks to the weird way that javascript handles arrays.
+	const subObj = obj[parts[0]];
+	if (subObj === undefined) throw new Error(`obj had no property ${parts[0]}`);
+	if (parts.length > 1) {
+		return getObjectProperty(subObj as {[name : string] : unknown}, parts.slice(1).join('.'));
+	}
+	return subObj;
+};

63a7dbc45743f4fa053442d977e0220e3b4ddbd5
Alex Komoroske <jkomoros@gmail.com>
2023-07-30 15:08:53 -0700

    Include `seedReference` in schema generation.
    Gives a slightly more readable schema.
    
    Part of #7.

--- tools/schema/main.ts
+++ tools/schema/main.ts
@@ -3,6 +3,7 @@ import { zodToJsonSchema } from 'zod-to-json-schema';
 import {
 	seedPacket,
 	seedData,
+	seedReference,
 	knownEnvironmentData
 } from '../../src/types.js';
 
@@ -16,7 +17,8 @@ const CONFIG_SCHEMA_FILE = 'config-schema.json';
 const writeSeed = () => {
 	const schema = zodToJsonSchema(seedPacket, {
 		definitions: {
-			seedData
+			seedData,
+			seedReference
 		}
 	});
 	writeFileSync(SEED_SCHEMA_FILE, JSON.stringify(schema, null, '\t'));

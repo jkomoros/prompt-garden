b9c61f88b8837a848865961026780f6244fdd637
Alex Komoroske <jkomoros@gmail.com>
2023-07-30 15:38:20 -0700

    Change output schema so it has a default tab completion.
    Part of #7.

--- tools/schema/main.ts
+++ tools/schema/main.ts
@@ -21,6 +21,23 @@ const writeSeed = () => {
 			seedReference
 		}
 	});
+
+	//Inject a default snippet so that when you start doing a seedData and hit tab it will fill in a default one.
+	//This is a VSCode-specific extension of JSON Schema: https://code.visualstudio.com/docs/languages/json#_file-match-syntax
+
+	if (!schema.definitions || !schema.definitions.seedData) throw new Error('Unexpected schema shape');
+
+	//eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const s = (schema.definitions.seedData as any);
+
+	s.defaultSnippets = [
+		{
+			body: {
+				t: '${1:noop}'
+			}
+		}
+	];
+
 	writeFileSync(SEED_SCHEMA_FILE, JSON.stringify(schema, null, '\t'));
 };
 

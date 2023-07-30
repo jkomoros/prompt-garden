jkomoros/prompt-garden

commit 45ccb02205b66d5316d73113bb6f8cf4353861de

Allow `input` seeds to have a `choices` which constraints the options.
Part of #18.

src/grow.ts
@@ -426,7 +426,12 @@ const growInput = async (seed : Seed<SeedDataInput>, env : Environment) : Promis
 	if (mock) {
 		return def;
 	}
-	return await seed.garden.profile.prompt(question, def);
+	const choices = await getProperty(seed, env, data.choices, undefined);
+	if (choices !== undefined) {
+		if (!Array.isArray(choices)) throw new Error('If choices is set, it must be an array');
+		if (choices.some(item => typeof item != 'string')) throw new Error('choices must be an array of strings');
+	}
+	return await seed.garden.profile.prompt(question, def, choices as string[]);
 };
 
 const growReference = async (seed : Seed<SeedDataReference>, env : Environment) : Promise<Value> => {

src/types.ts
@@ -663,7 +663,8 @@ const seedDataConfigInput = {
 	type: z.literal('input'),
 	properties: {
 		question: z.string().describe('The question to ask the user'),
-		default: z.string().optional().describe('The value to use as default if the user doesn\'t provide anything else')
+		default: z.string().optional().describe('The value to use as default if the user doesn\'t provide anything else'),
+		choices: z.array(z.string()).optional().describe('The choices to limit to')
 	}
 };
 

commit 8c026e3c4f4170c33ae6d8ee0deefed6c4d315e8

Fixed a bug in last commit where an empty choices would complain.
Bug introduced in 45ccb02205b66d5316d73113bb6f8cf4353861de

Part of #18.

src/grow.ts
@@ -426,12 +426,12 @@ const growInput = async (seed : Seed<SeedDataInput>, env : Environment) : Promis
 	if (mock) {
 		return def;
 	}
-	const choices = await getProperty(seed, env, data.choices, undefined);
-	if (choices !== undefined) {
+	const choices = await getProperty(seed, env, data.choices, null);
+	if (choices !== null) {
 		if (!Array.isArray(choices)) throw new Error('If choices is set, it must be an array');
 		if (choices.some(item => typeof item != 'string')) throw new Error('choices must be an array of strings');
 	}
-	return await seed.garden.profile.prompt(question, def, choices as string[]);
+	return await seed.garden.profile.prompt(question, def, choices ? choices as string[] : undefined);
 };
 
 const growReference = async (seed : Seed<SeedDataReference>, env : Environment) : Promise<Value> => {
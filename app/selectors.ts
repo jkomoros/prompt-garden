8ac5244b7c3788920ff8280d35c0a4b9dd4775db
Alex Komoroske <jkomoros@gmail.com>
2023-08-06 15:35:20 -0700

    Create selectCurrentPacket.
    Part of #6.

--- app/selectors.ts
+++ app/selectors.ts
@@ -1,4 +1,8 @@
+import { createSelector } from 'reselect';
+
 import {
+	PacketName,
+	Packets,
 	RootState,
 } from './types.js';
 
@@ -9,3 +13,9 @@ export const selectHash = (state : RootState) => state.app ? state.app.hash : ''
 export const selectCurrentPacketName = (state : RootState) => state.data ? state.data.currentPacket : '';
 export const selectCurrentSeedID = (state : RootState) => state.data ? state.data.currentSeed : '';
 export const selectPackets = (state : RootState) => state.data ? state.data.packets : {};
+
+export const selectCurrentPacket = createSelector(
+	selectCurrentPacketName,
+	selectPackets,
+	(name : PacketName, packets : Packets) => packets[name]
+);

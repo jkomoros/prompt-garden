9ffbd4f552f13104c1aa874e1b8f4b4b88523bc0
Alex Komoroske <jkomoros@gmail.com>
2023-08-06 16:23:10 -0700

    Add a json-editor stub.
    Part of #6.

---
+++ app/components/json-editor.ts
@@ -0,0 +1,46 @@
+import { LitElement } from 'lit';
+import { property, customElement } from 'lit/decorators.js';
+import { html, TemplateResult} from 'lit';
+
+import {
+	SharedStyles
+} from './shared-styles.js';
+
+import {
+	ButtonSharedStyles
+} from './button-shared-styles.js';
+
+import {
+	ObjectPath
+} from '../types.js';
+
+@customElement('json-editor')
+export class JSONEditor extends LitElement {
+
+	@property({type:Object})
+		data: unknown = {};
+
+	@property({type: Array})
+		path: ObjectPath = [];
+
+	static override get styles() {
+		return [
+			SharedStyles,
+			ButtonSharedStyles,
+		];
+	}
+
+	override render() : TemplateResult {
+		return html`
+		<pre>${JSON.stringify(this.data, null, '\t')}</pre>			
+		`;
+	}
+
+
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'json-editor': JSONEditor;
+	}
+}

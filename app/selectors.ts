12e856afa714e7f62310dafc0261d8e074f6a54a
Alex Komoroske <jkomoros@gmail.com>
2023-08-06 14:26:07 -0700

    Add a selectPackets selector.
    Part of #6.

--- app/selectors.ts
+++ app/selectors.ts
@@ -5,3 +5,5 @@ import {
 export const selectPage = (state : RootState) => state.app ? state.app.page : '';
 export const selectPageExtra = (state : RootState) => state.app ? state.app.pageExtra : '';
 export const selectHash = (state : RootState) => state.app ? state.app.hash : '';
+
+export const selectPackets = (state : RootState) => state.data ? state.data.packets : {};

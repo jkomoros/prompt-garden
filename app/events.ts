1235bfbf56936635e4fd8b3454a41a7c951f4bdd
Alex Komoroske <jkomoros@gmail.com>
2023-08-06 15:15:16 -0700

    Add a CreatePacketEvent.
    Part of #6.

--- app/events.ts
+++ app/events.ts
@@ -3,6 +3,7 @@ import {
 } from './types.js';
 
 const CURRENT_PACKET_CHANGED_EVENT_NAME = 'current-packet-changed';
+const CREATE_PACKET_NAME = 'create-packet';
 
 type CurrentPacketEventDetail = {
 	name: PacketName
@@ -12,4 +13,10 @@ export type CurrentPacketChangedEvent = CustomEvent<CurrentPacketEventDetail>;
 
 export const makeCurrentPacketChangedEvent = (packetName : PacketName) : CurrentPacketChangedEvent => {
 	return new CustomEvent(CURRENT_PACKET_CHANGED_EVENT_NAME, {composed: true, detail: {name: packetName}});
+};
+
+export type CreatePacketEvent = CustomEvent<null>;
+
+export const makeCreatePacketEvent = () : CreatePacketEvent => {
+	return new CustomEvent(CREATE_PACKET_NAME, {composed: true});
 };

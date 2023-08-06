a870e663b88f7e4ec8cce81ffa048265c2399065
Alex Komoroske <jkomoros@gmail.com>
2023-08-06 15:50:34 -0700

    packet-editor accepts a seed-id.
    Part of #6.

--- app/components/packet-editor.ts
+++ app/components/packet-editor.ts
@@ -26,6 +26,12 @@ import {
 	PLUS_ICON
 } from './my-icons.js';
 
+import {
+	SeedID,
+	SeedPacket,
+	emptySeedPacket
+} from '../../src/types.js';
+
 @customElement('packet-editor')
 export class PacketEditor extends LitElement {
 
@@ -34,6 +40,9 @@ export class PacketEditor extends LitElement {
 
 	@property({type: String})
 		currentPacketName : PacketName = '';
+
+	@property({type: String})
+		currentSeedID: SeedID = '';
 
 	static override get styles() {
 		return [
@@ -46,11 +55,17 @@ export class PacketEditor extends LitElement {
 		return html`
 			<div class='container'>
 				<div class='controls'>
+					<label>Packets</label>
 					<select .value=${this.currentPacketName} @change=${this._handleCurrentPacketChanged}>
 						${Object.keys(this.packets).map(name => html`<option .value='${name}' .selected=${name == this.currentPacketName}>${name}</option>`)}
 					</select>
 					<button class='small' @click=${this._handleCreatePacket} title='Create packet'>${PLUS_ICON}</button>
 					<button class='small' @click=${this._handleDeletePacket} title='Delete packet'>${DELETE_FOREVER_ICON}</button>
+					<label>Seeds</label>
+					<select .value=${this.currentSeedID} @change=${this._handleCurrentSeedChanged}>
+						${Object.keys(this.currentPacket.seeds).map(id => html`<option .value='${id}' .selected=${id == this.currentSeedID}>${id}</option>`)}
+					</select>
+					
 				</div>
 				<pre>${JSON.stringify(this.packets[this.currentPacketName], null, '\t')}</pre>
 			</div>
@@ -58,10 +73,20 @@ export class PacketEditor extends LitElement {
 		`;
 	}
 
+	get currentPacket() : SeedPacket {
+		return this.packets[this.currentPacketName] || emptySeedPacket();
+	}
+
 	_handleCurrentPacketChanged(e : Event) {
 		const ele = e.composedPath()[0];
 		if (!(ele instanceof HTMLSelectElement)) throw new Error('not select element');
 		this.dispatchEvent(makeCurrentPacketChangedEvent(ele.value));
+	}
+
+	_handleCurrentSeedChanged(e : Event) {
+		const ele = e.composedPath()[0];
+		if (!(ele instanceof HTMLSelectElement)) throw new Error('not select element');
+		console.log(`TODO: actually emit an event to change to seed ${ele.value}`);
 	}
 
 	_handleCreatePacket() {

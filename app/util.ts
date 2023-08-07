a9eb8ddb3eb0a375032296d6d43f24d0a8350fa6
Alex Komoroske <jkomoros@gmail.com>
2023-08-07 06:45:34 -0700

    define getProperty which takes an object and property path.
    Not yet tested.
    
    Part of #6.

--- app/util.ts
+++ app/util.ts
@@ -19,3 +19,21 @@ export const storePacketsToStorage = (packets : Packets) => {
 export const unpackDottedPath = (path : DottedObjectPath) : ObjectPath => {
 	return path.split('.').map(piece => isNaN(parseInt(piece)) ? piece : parseInt(piece));
 };
+
+type PropertyInput = Record<string, unknown> | unknown[];
+
+export const getProperty = (obj : PropertyInput, path : ObjectPath) : unknown => {
+	if (!obj || typeof obj != 'object') throw new Error('obj must be an object');
+	const pathPart = path[0];
+	const pathRest = path.slice(1);
+	const isArray = Array.isArray(obj);
+	const isNumber = typeof pathPart == 'number';
+	if (isArray && !isNumber) throw new Error('Obj is array but path part is not number');
+	if (!isArray && isNumber) throw new Error('Obj is not array but path part is number');
+	const subObj = (obj as Record<string, unknown>)[pathPart];
+	if (!subObj || typeof subObj != 'object') throw new Error('Sub-obj is not an object');
+	if (path.length > 1) {
+		return getProperty(subObj as Record<string, unknown>, pathRest);
+	}
+	return subObj;
+};

75a3abf43cf3291b486fbb891bf08d875b1ba4f9
Alex Komoroske <jkomoros@gmail.com>
2023-08-13 14:40:53 -0700

    Add a (currently unused) dialog-element.
    A copy/paste from adjacency-map.
    
    Part of #6.

---
+++ app/components/dialog-element.ts
@@ -0,0 +1,214 @@
+import { LitElement, html, css, TemplateResult } from 'lit';
+
+import { customElement, property } from 'lit/decorators.js';
+
+import { SharedStyles } from './shared-styles.js';
+
+import { ButtonSharedStyles } from './button-shared-styles.js';
+
+import {
+	CANCEL_ICON
+} from './my-icons.js';
+
+@customElement('dialog-element')
+export class DialogElement extends LitElement {
+
+	@property({ type : Boolean })
+		open = false;
+
+	@property({ type : String })
+	override title = '';
+
+	@property({ type : Boolean })
+		mobile = false;
+
+	@property({ type : Boolean})
+		hideClose = false;
+
+	static override get styles() {
+		return [
+			css`
+			.container {
+				position: absolute;
+				height: 100%;
+				width: 100%;
+				top: 0;
+				left: 0;
+				/* Note that card-preview has a z-index higher than this to
+				show up above it */
+				z-index: 1000;
+				display: none;
+			}
+
+			.container.open {
+				display: block;
+			}
+
+			.background {
+				position:absolute;
+				height:100%;
+				width:100%;
+				top:0;
+				left:0;
+				background-color:#FFFFFFCC;
+				display:flex;
+				flex-direction:column;
+				align-items: center;
+				justify-content:center;
+			}
+
+			.content {
+				background-color:white;
+				padding:1em;
+				box-sizing: border-box;
+				box-shadow: 0 2px 6px #CCC;
+				position:relative;
+				display:flex;
+				flex-direction:column;
+				min-height: 40%;
+				min-width: 40%;
+				max-height:90%;
+				max-width:70%;
+			}
+
+			.mobile .content {
+				height:100%;
+				width:100%;
+				max-height:none;
+				max-width:none;
+			}
+
+			h2 {
+				font-weight: normal;
+				font-size:1.5em;
+				text-align:left;
+				margin:0;
+			}
+
+			#close {
+				position: absolute;
+				top: 0.5em;
+				right: 0.5em;
+			}
+
+			#inner {
+				flex-grow:1;
+				display:flex;
+				flex-direction:column;
+				overflow:scroll;
+			}
+
+			.buttons {
+					display: flex;
+					justify-content: flex-end;
+			}
+
+			`,
+			SharedStyles,
+			ButtonSharedStyles
+		];
+	}
+
+	constructor() {
+		super();
+		this.hideClose = false;
+	}
+
+	override render() : TemplateResult {
+		return html`
+			<div class='container ${this.open ? 'open' : 'closed'}'>
+				<div class='background ${this.mobile ? 'mobile': ''}' @click=${this._handleBackgroundClicked}>
+					<div class='content'>
+						${this.hideClose ? '' : html`<button class='small' id='close' @click=${this.cancel}>${CANCEL_ICON}</button>`}
+						<h2>${this.title || ''}</h2>
+						<div id='inner'>
+						${this.innerRender()}
+						</div>
+						<div class='buttons'>
+						${this.buttonsRender()}
+						</div>
+					</div>
+				</div>
+			</div>
+	`;
+	}
+
+	innerRender() : TemplateResult {
+		//You can subclass this and return somethingelse for innerRender or use it directly with content inside.
+		return html`<slot></slot>`;
+	}
+
+	buttonsRender() : TemplateResult {
+		//You can subclass this and return somethingelse for innerRender or use it directly with content inside.
+		return html`<slot name='buttons'></slot>`;
+	}
+
+	override firstUpdated() {
+		window.addEventListener('keydown', e => this._handleKeyDown(e));
+	}
+
+	_handleKeyDown(e : KeyboardEvent) : void {
+		if (!this.open) return;
+		if (e.key == 'Escape') {
+			this.cancel();
+		}
+	}
+
+	_handleBackgroundClicked(e : MouseEvent) : void {
+		const background = this._root.querySelector('.background');
+		//If the click wasn't actualy directly on the background then ignore it.
+		if (e.composedPath()[0] != background) return;
+		this._shouldClose();
+	}
+
+	get _root() : ShadowRoot {
+		return this.shadowRoot as ShadowRoot;
+	}
+
+	cancel() {
+		this._shouldClose(true);
+	}
+
+	_shouldClose(cancelled = false) {
+		//Override point for sub classes
+		this.dispatchEvent(new CustomEvent('dialog-should-close', {composed: true, detail: {cancelled}}));
+	}
+
+	_focusInputOnOpen() {
+		//Override point for sub classes
+
+		//Make sure if there's a text field it's focused.
+
+		let input = this._root.querySelector('input[type=text]');
+		if (!input) input = this._root.querySelector('input[type=search]');
+		if (!input) input = this._root.querySelector('textarea');
+		if (!input) {
+			const slot = this._root.querySelector('slot');
+			if (slot){
+				for (const ele of slot.assignedElements()) {
+					if (ele.localName == 'textarea') {
+						input = ele;
+						break;
+					}
+				}
+			}
+		}
+		if (!input) return;
+		if (!(input instanceof HTMLInputElement)) throw new Error('not input ele');
+		input.focus();
+		input.select();
+	}
+
+	override updated(changedProps : Map<string, DialogElement[keyof DialogElement]>) {
+		if (changedProps.has('open') && this.open) {
+			this._focusInputOnOpen();
+		}
+	}
+
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'dialog-element': DialogElement;
+	}
+}

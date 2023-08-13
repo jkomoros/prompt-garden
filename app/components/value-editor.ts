jkomoros/prompt-garden

commit 7999397b9ed312d9d3672ef2139dbf705b9da3ae

render of value-editor sets an inner.
Soon we'll prepend a control.

Part of #6.

app/components/value-editor.ts
@@ -127,9 +127,11 @@ export class ValueEditor extends LitElement {
 
 	override render() : TemplateResult {
 
+		let inner = html``;
+
 		if (this.choices) {
 			if (typeof this.data != 'string') throw new Error('choices provided but data is not string');
-			return html`<select .value=${this.data} @change=${this._handlePropertyChanged}>
+			inner = html`<select .value=${this.data} @change=${this._handlePropertyChanged}>
 			${this.choices.map(choice => html`<option .value=${choice} .selected=${this.data == choice}>${choice}</option>`)}
 			</select>`;
 		}
@@ -138,21 +140,28 @@ export class ValueEditor extends LitElement {
 
 		switch(typ) {
 		case 'string':
-			return html`<input type='text' .value=${this.data as string} @change=${this._handlePropertyChanged}></input>`;
+			inner = html`<input type='text' .value=${this.data as string} @change=${this._handlePropertyChanged}></input>`;
+			break;
 		case 'number':
-			return html`<input type='number' .value=${String(this.data)} @change=${this._handlePropertyChanged}></input>`;
+			inner = html`<input type='number' .value=${String(this.data)} @change=${this._handlePropertyChanged}></input>`;
+			break;
 		case 'boolean':
-			return html`<input type='checkbox' .checked=${this.data as boolean} @change=${this._handlePropertyChanged}></input>`;
+			inner = html`<input type='checkbox' .checked=${this.data as boolean} @change=${this._handlePropertyChanged}></input>`;
+			break;
 		case 'seed':
-			return html`<seed-editor .data=${this.data} .path=${this.path}></seed-editor>`;
+			inner = html`<seed-editor .data=${this.data} .path=${this.path}></seed-editor>`;
+			break;
 		case 'reference':
 		case 'array':
 		case 'object':
 			//TODO: have a special seed-reference-editor.
-			return html`${Object.entries(this.data as Record<string, unknown>).map(entry => html`<div class='row'><label>${entry[0]}</label><value-editor .path=${[...this.path, entry[0]]} .data=${entry[1]}></value-editor></div>`)}`;
+			inner = html`${Object.entries(this.data as Record<string, unknown>).map(entry => html`<div class='row'><label>${entry[0]}</label><value-editor .path=${[...this.path, entry[0]]} .data=${entry[1]}></value-editor></div>`)}`;
+			break;
 		default:
-			return assertUnreachable(typ);
+			assertUnreachable(typ);
 		}
+
+		return html`${inner}`;
 	}
 
 	_handlePropertyChanged(e : Event) {

commit 0b251e888783493f132c16328fa017c26649f963

Fix it so choices renders a dropdown.
broken in 7999397b9ed312d9d3672ef2139dbf705b9da3ae, because inner was re-set later.

Part of #6.

app/components/value-editor.ts
@@ -131,13 +131,6 @@ export class ValueEditor extends LitElement {
 
 		let inner = html``;
 
-		if (this.choices) {
-			if (typeof this.data != 'string') throw new Error('choices provided but data is not string');
-			inner = html`<select .value=${this.data} @change=${this._handlePropertyChanged}>
-			${this.choices.map(choice => html`<option .value=${choice} .selected=${this.data == choice}>${choice}</option>`)}
-			</select>`;
-		}
-
 		const typ = dataType(this.data);
 
 		switch(typ) {
@@ -161,6 +154,13 @@ export class ValueEditor extends LitElement {
 			break;
 		default:
 			assertUnreachable(typ);
+		}
+
+		if (this.choices) {
+			if (typeof this.data != 'string') throw new Error('choices provided but data is not string');
+			inner = html`<select .value=${this.data} @change=${this._handlePropertyChanged}>
+			${this.choices.map(choice => html`<option .value=${choice} .selected=${this.data == choice}>${choice}</option>`)}
+			</select>`;
 		}
 
 		const select = this.disallowTypeChange ? html`` : html`<select .value=${typ} @change=${this._handleTypeChanged}>
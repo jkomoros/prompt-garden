2d2cba616282afdc1990f602ffea37f532ca2fd8
Alex Komoroske <jkomoros@gmail.com>
2023-08-20 06:03:23 -0700

    runSeed action actually fetches and executes the seed.
    Not yet exercised in the UI.
    
    Part of #6.

--- app/actions/garden.ts
+++ app/actions/garden.ts
@@ -14,11 +14,31 @@ import {
 	PacketName
 } from '../types.js';
 
-export const runSeed = (packetName : PacketName, seedID : SeedID) : ThunkResult => (dispatch) =>  {
+import {
+	selectGarden
+} from '../selectors.js';
+
+export const runSeed = (packetName : PacketName, seedID : SeedID) : ThunkResult => async (dispatch, getState) =>  {
 	dispatch({
 		type: START_SEED,
 		packetName,
 		seedID
 	});
-	throw new Error('Actually running a seed is not yet supported');
+	const state = getState();
+	const garden = selectGarden(state);
+
+	//TODO: catch thrown errors too
+	const seed = await garden.seed({seed: seedID, packet: packetName});
+	if (!seed) {
+		dispatch({
+			type: SEED_ERRORED,
+			error: `Couldn't find seed ${seedID} in packet ${packetName}`
+		});
+		return;
+	}
+	const result = await seed.grow();
+	dispatch({
+		type: SEED_FINISHED,
+		result
+	});
 };

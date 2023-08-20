424f29cd2f8f88933a15346eaa139530c4abd01e
Alex Komoroske <jkomoros@gmail.com>
2023-08-20 15:03:23 -0700

    seed-list can take a currentSeedID and currentPacketName
    for the items that match, it renders a selected class.
    
    Part of #6.

--- app/components/seed-list.ts
+++ app/components/seed-list.ts
@@ -1,5 +1,6 @@
-import { LitElement } from 'lit';
+import { LitElement, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
 import { html, TemplateResult} from 'lit';
 
 import {
@@ -11,6 +12,7 @@ import {
 } from './button-shared-styles.js';
 
 import {
+	SeedID,
 	SeedPacket
 } from '../../src/types.js';
 
@@ -28,11 +30,22 @@ export class SeedList extends LitElement {
 
 	@property({type:Object})
 		packets: Packets = {};
+
+	@property({type: String})
+		currentPacketName: PacketName = '';
+	
+	@property({type: String})
+		currentSeedID: SeedID = '';
 	
 	static override get styles() {
 		return [
 			SharedStyles,
 			ButtonSharedStyles,
+			css`
+				.selected {
+					font-weight: bold;
+				}
+			`
 		];
 	}
 
@@ -44,10 +57,20 @@ export class SeedList extends LitElement {
 
 	_controlForPacket(name : PacketName, packet : SeedPacket) : TemplateResult {
 		//TODO: keep track of which details are opened
+		const classes = {
+			selected: name == this.currentPacketName
+		};
 		return html`<details>
-				<summary>${name}</summary>
-				${Object.keys(packet.seeds).map(seedID => html`<div class='row'>${seedID}</div>`)}
+				<summary class=${classMap(classes)}>${name}</summary>
+				${Object.keys(packet.seeds).map(seedID => this._controlForSeed(name, seedID))}
 		</details>`;
+	}
+
+	_controlForSeed(packetName : PacketName, seedID : SeedID) : TemplateResult {
+		const classes = {
+			selected: packetName == this.currentPacketName && seedID == this.currentSeedID
+		};
+		return html`<div class=${classMap(classes)}>${seedID}</div>`;
 	}
 
 

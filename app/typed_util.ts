d2ba983f4dfbf7e6849650e4baa2fc749e49c048
Alex Komoroske <jkomoros@gmail.com>
2023-08-26 06:32:16 -0700

    Add a packetTypeEditable utility function.
    Instead of checking for `local`, we can use this giving one future proof extension point.
    
    Part of #6.

--- app/typed_util.ts
+++ app/typed_util.ts
@@ -13,6 +13,17 @@ import {
 	PacketsBundle
 } from './types.js';
 
+export const packetTypeEditable = (packetType : PacketType) : boolean => {
+	switch(packetType) {
+	case 'local':
+		return true;
+	case 'remote':
+		return false;
+	default:
+		return assertUnreachable(packetType);
+	}
+};
+
 export const getPacket = (bundle : PacketsBundle, name : PacketName, packetType : PacketType) : SeedPacket => {
 
 	let packets : Packets = {};

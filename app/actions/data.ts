jkomoros/prompt-garden

commit a32283fa692be34975767a09f098231eda454ea6

When changing an environment property, we first validate that it will be legal and drop the update if it's not.
Part of #6.

app/actions/data.ts
@@ -14,7 +14,6 @@ import {
 	CHANGE_PROPERTY,
 	DELETE_PROPERTY,
 	ActionLoadEnvironment,
-	ActionChangeEnvironmentProperty,
 	ActionLoadPackets,
 	SET_PACKET_COLLAPSED,
 } from '../actions.js';
@@ -41,6 +40,7 @@ import {
 } from '../selectors.js';
 
 import {
+	environmentData,
 	EnvironmentData,
 	SeedID,
 	seedPacket,
@@ -71,6 +71,10 @@ import {
 	TypedObject
 } from '../../src/typed-object.js';
 
+import {
+	ZodError
+} from 'zod';
+
 export const loadEnvironment = (environment : EnvironmentData) : ActionLoadEnvironment => {
 	return {
 		type: LOAD_ENVIRONMENT,
@@ -78,8 +82,27 @@ export const loadEnvironment = (environment : EnvironmentData) : ActionLoadEnvir
 	};
 };
 
-export const changeEnvironmentProperty = (key : string, rawValue: unknown) : ActionChangeEnvironmentProperty => {
+export const changeEnvironmentProperty = (key : string, rawValue: unknown) : ThunkSomeAction => (dispatch, getState) => {
+	const state = getState();
 	const v = value.parse(rawValue);
+
+	const newEnv = {
+		...selectEnvironmentData(state),
+		[key]: v
+	};
+
+	try {
+		//Throw if the new value is not valid.
+		environmentData.parse(newEnv);
+	} catch(err) {
+		if (err instanceof ZodError) {
+			alert(`Could not set value: ${err.errors[0].message}`);
+		} else {
+			alert(err);
+		}
+		return;
+	}
+
 	return{
 		type: CHANGE_ENVIRONMENT_PROPERTY,
 		key,

app/actions.ts
@@ -105,8 +105,6 @@ const actionChangeEnvironmentProperty = z.object({
 	value: value
 }).strict();
 
-export type ActionChangeEnvironmentProperty = z.infer<typeof actionChangeEnvironmentProperty>;
-
 const actionDeleteEnvironmentProperty = z.object({
 	type: z.literal(DELETE_ENVIRONMENT_PROPERTY),
 	key: z.string()

commit 29c2dcfb191625d218713575a15ca85bb8015902

Fix a bug broken in a32283fa692be34975767a09f098231eda454ea6
We were returning a value, not dispatching when we changed to thunk.

Part of #6.

app/actions/data.ts
@@ -103,11 +103,11 @@ export const changeEnvironmentProperty = (key : string, rawValue: unknown) : Thu
 		return;
 	}
 
-	return{
+	dispatch({
 		type: CHANGE_ENVIRONMENT_PROPERTY,
 		key,
 		value: v
-	};
+	});
 };
 
 export const deleteEnvironmentProperty = (key : string) : ThunkSomeAction => (dispatch, getState) => {
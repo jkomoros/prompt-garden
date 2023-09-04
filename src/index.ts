7f6aad21b14572d347e72c6d157d9bf241221c5e
Alex Komoroske <jkomoros@gmail.com>
2023-09-04 15:13:00 -0700

    Add a starter index.ts for the published APIs.
    Part of #64.

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+//This is the primary export point for the library.
+
+import {
+	Garden
+} from './garden.js';
+
+import {
+	Environment
+} from './environment.js';
+
+import {
+	Profile
+} from './profile.js';
+
+import {
+	Template
+} from './template.js';
+
+export {
+	Garden,
+	Environment,
+	Profile,
+	Template
+};

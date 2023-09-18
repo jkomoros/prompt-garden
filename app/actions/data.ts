jkomoros/prompt-garden

commit 3fb6b15f7014719d3906231c7dd225867052c016

Add a savePacket action creator.
Not yet wired into the UI.

Part of #6.

app/actions/data.ts
@@ -35,6 +35,7 @@ import {
 
 import {
 	selectAllowEditing,
+	selectCurrentPacket,
 	selectCurrentPacketName,
 	selectCurrentPacketType,
 	selectCurrentSeed,
@@ -81,6 +82,7 @@ import {
 import {
 	ZodError
 } from 'zod';
+import { saveAs } from 'file-saver';
 
 export const loadEnvironment = (environment : EnvironmentData) : ActionLoadEnvironment => {
 	return {
@@ -509,4 +511,13 @@ export const firstRunIfNecessary = () : ThunkSomeAction => async (dispatch) => {
 	await dispatch(createNamedPacket('starter'));
 	await dispatch(createNamedSeed('starter', 'example'));
 	setFirstRunComplete();
+};
+
+export const savePacket = () : ThunkSomeAction => (_, getState) => {
+	const state = getState();
+	const rawPacketName = selectCurrentPacketName(state);
+	const packetName = rawPacketName.toLowerCase().endsWith('.json') ? rawPacketName : rawPacketName + '.json';
+	const packet = selectCurrentPacket(state);
+	const data = JSON.stringify(packet.data, null, '\t');
+	saveAs(data, packetName);
 };

commit b2bb7bbf3f589c98e64f57babc2de7dae18acb67

Fix a webapp error introduced in 3fb6b15f7014719d3906231c7dd225867052c016 with file-saver import done wrong.
I will note that I imported it exactly like the documentation said...

It still doesn't work because the file download doesn't work.

Part of #6.

app/actions/data.ts
@@ -81,7 +81,8 @@ import {
 import {
 	ZodError
 } from 'zod';
-import { saveAs } from 'file-saver';
+
+import fileSaver from 'file-saver';
 
 export const loadEnvironment = (environment : EnvironmentData) : ActionLoadEnvironment => {
 	return {
@@ -519,5 +520,5 @@ export const downloadPacket = (packetName : PacketName, packetType : PacketType)
 	if (!packet) throw new Error(`No such packet: ${packetName}:${packetType}`);
 	const finalPacketName = packetName.toLowerCase().endsWith('.json') ? packetName : packetName + '.json';
 	const data = JSON.stringify(packet.data, null, '\t');
-	saveAs(data, finalPacketName);
+	fileSaver.saveAs(data, finalPacketName);
 };

web-dev-server.config.mjs
@@ -21,6 +21,7 @@ export default {
 			include: [
 				// the commonjs plugin is slow, list the required packages explicitly:
 				'**/node_modules/gpt-tok/**/*',
+				'**/node_modules/file-saver/**/*'
 			]
 		})
 	]
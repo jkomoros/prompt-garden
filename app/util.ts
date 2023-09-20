jkomoros/prompt-garden

commit dd89c1aa29b9e60991ee0d2107b522dd0b08701a

Made pathToPacketName and packetNameToPath handle relative and absolute paths.
This makes it so the drop down of imported paths work (previously we blindly prepended './', now we
only do if the packetName doesn't already have a path).

Part of #6.

app/util.ts
@@ -13,7 +13,6 @@ import {
 	emptySeedPacket,
 	SeedDataIsh,
 	SeedID,
-	SeedPacketRelativeLocation,
 	SeedPacketLocation
 } from '../src/types.js';
 
@@ -35,18 +34,23 @@ import {
 	assertUnreachable
 } from '../src/util.js';
 
+import {
+	isLocation
+} from '../src/reference.js';
+
 const PACKETS_LOCAL_STORAGE_KEY = 'packets';
 const ENVIRONMENT_LOCAL_STORAGE_KEY = 'environment';
 const INITALIZED_LOCAL_STORAGE_KEY = 'initialized';
 
 const RELATIVE_PATH_PREFIX = './';
 
-export const relativePathToPacketName = (relativePath : SeedPacketLocation) : PacketName => {	
-	if (relativePath.startsWith(RELATIVE_PATH_PREFIX)) return relativePath.slice(RELATIVE_PATH_PREFIX.length);
-	return relativePath;
+export const pathToPacketName = (path : SeedPacketLocation) : PacketName => {	
+	const pathParts = path.split('/');
+	return pathParts[pathParts.length - 1];
 };
 
-export const packetNameToRelativePath = (packetName : PacketName) : SeedPacketRelativeLocation => {
+export const packetNameToPath = (packetName : PacketName) : SeedPacketLocation => {
+	if (isLocation(packetName)) return packetName;
 	return RELATIVE_PATH_PREFIX + packetName;
 };
 

app/components/seed-reference-editor.ts
@@ -32,8 +32,8 @@ import {
 import {
 	getAllPacketNames,
 	getPacketOfUnknownType,
-	packetNameToRelativePath,
-	relativePathToPacketName
+	packetNameToPath,
+	pathToPacketName
 } from '../util.js';
 
 const CUSTOM_SENTINEL = '@CUSTOM@';
@@ -74,7 +74,7 @@ export class SeedReferenceEditor extends LitElement {
 
 	override render() : TemplateResult {
 		const reference = this.reference || emptySeedReference();
-		const referencePacket = relativePathToPacketName(reference.packet || '');
+		const referencePacket = pathToPacketName(reference.packet || '');
 		const packetName = reference.packet === undefined ? this.currentSeedSelector.packetName : referencePacket;
 		const packet = getPacketOfUnknownType(this.packets, packetName);
 
@@ -167,7 +167,7 @@ export class SeedReferenceEditor extends LitElement {
 				value = prompt(question, '') || '';
 			}
 		} else {
-			value = packetNameToRelativePath(value);
+			value = packetNameToPath(value);
 		}
 		this.dispatchEvent(makePropertyChangedEvent([...this.path, PACKET_PROPERTY], value));
 	}

commit c1c55aef91206a30c8c0cb667d7903d8d1a5c3e1

Fixed a bug introduced in dd89c1aa29b9e60991ee0d2107b522dd0b08701a where remote packets were getting the wrong name.
The fix is a bit confusing but it's how remote and local packet names work.

Part of #6.

app/util.ts
@@ -35,6 +35,7 @@ import {
 } from '../src/util.js';
 
 import {
+	isLocalLocation,
 	isLocation
 } from '../src/reference.js';
 
@@ -44,7 +45,9 @@ const INITALIZED_LOCAL_STORAGE_KEY = 'initialized';
 
 const RELATIVE_PATH_PREFIX = './';
 
-export const pathToPacketName = (path : SeedPacketLocation) : PacketName => {	
+export const pathToPacketName = (path : SeedPacketLocation) : PacketName => {
+	//This is kind of hacky but remote packets in remotes start with https://
+	if (!isLocalLocation(path)) return path;
 	const pathParts = path.split('/');
 	return pathParts[pathParts.length - 1];
 };
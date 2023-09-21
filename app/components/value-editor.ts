jkomoros/prompt-garden

commit 0c3c0804336424bc31bf94f3eb5687fdb831b33a

Render a text-area for multi-line properties.
The way value-editor decides how to render a given control is based on the type, which can't tell us if it's
multiLine. So we take a hint from the seed-editor caller context to tell us we should be a multiLine if
we're a string.

Part of #6.

app/components/value-editor.ts
@@ -86,6 +86,9 @@ export class ValueEditor extends LitElement {
 	@property({type: Boolean})
 		disallowDelete = false;
 
+	@property({type: Boolean})
+		multiLine = false;
+
 	static override get styles() {
 		return [
 			SharedStyles,
@@ -110,13 +113,22 @@ export class ValueEditor extends LitElement {
 
 		switch(typ) {
 		case 'string':
-			inner = html`<input
+			if (this.multiLine) {
+				inner = html`<textarea
+					.value=${this.data as string}
+					@change=${this._handlePropertyChanged}
+					?disabled=${!this.editable}
+					>
+				</textarea>`;
+			} else {
+				inner = html`<input
 					type='text'
 					.value=${this.data as string}
 					@change=${this._handlePropertyChanged}
 					?disabled=${!this.editable}
 				>
 				</input>`;
+			}
 			break;
 		case 'number':
 			inner = html`<input

app/components/seed-editor.ts
@@ -214,6 +214,7 @@ export class SeedEditor extends LitElement {
 						.prompter=${this.prompter}
 						.packets=${this.packets}
 						.currentSeedSelector=${this.currentSeedSelector}
+						.multiLine=${propShape.multiLine}
 						@property-changed=${hookTypeChangedEvent ? this._handleSubTypeChanged : this._handleNormalPropertyChanged}>
 					</value-editor>
 				</div>`;

commit d5c738eb394dc71021230a681417e0b709d946a1

Fix a bug broken in 0c3c0804336424bc31bf94f3eb5687fdb831b33a where multiLine text didn't actually update the model.
Part of #6.

app/components/value-editor.ts
@@ -283,7 +283,7 @@ export class ValueEditor extends LitElement {
 
 	_handlePropertyChanged(e : Event) {
 		const ele = e.composedPath()[0];
-		if (!(ele instanceof HTMLSelectElement) && !(ele instanceof HTMLInputElement)) throw new Error('not select or input element');
+		if (!(ele instanceof HTMLSelectElement) && !(ele instanceof HTMLInputElement) && !(ele instanceof HTMLTextAreaElement)) throw new Error('not select or input element');
 		let value : string | boolean | number  = (ele instanceof HTMLInputElement && ele.type == 'checkbox') ? ele.checked : ele.value;
 		if (propertyType(this.data) == 'number') value = parseFloat(value as string);
 		this.dispatchEvent(makePropertyChangedEvent(this.path, value));
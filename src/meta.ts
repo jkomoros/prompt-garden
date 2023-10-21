jkomoros/prompt-garden

commit 0e1d9fa049e6aac2efad8a64b33a6f7544f8dc2f

make choicesAsStrings not return undefined.
Part of #6.

src/meta.ts
@@ -138,8 +138,8 @@ TODO (plane):
   (make sure it still allows seeds and references)
 */
 
-export const choicesAsStrings = (choices? : Choice[]) : string[] | undefined => {
-	if (!choices) return undefined;
+export const choicesAsStrings = (choices? : Choice[]) : string[] => {
+	if (!choices) return [];
 	return choices.map(choice => {
 		if (typeof choice == 'string') return choice;
 		return choice.value;

src/profile.ts
@@ -5,7 +5,10 @@ import {
 import {
 	Garden
 } from './garden.js';
-import { choicesAsStrings } from './meta.js';
+
+import {
+	choicesAsStrings
+} from './meta.js';
 
 import {
 	EmbeddingModelID,
@@ -158,7 +161,7 @@ export class Profile{
 		const def = String(defaultValue);
 		if (!choices) return prompt(question, def) || '';
 
-		const strChoices = choicesAsStrings(choices) || [];
+		const strChoices = choicesAsStrings(choices);
 
 		const finalQuestion = question + '\n\nChoices:\n' + strChoices.join('\n');
 

commit 47eb4f1516c3122817ecb0cb696a9c14cf26a04a

Fix environment-editor for string keys.
It was broken in 0e1d9fa049e6aac2efad8a64b33a6f7544f8dc2f because choicesAsStrings is actually used in more places than I realized, and it required being able to return undefined.

After that change broke it, you couldn't add an openai_api_key in the environment editor because it would render a blank drop down.

Part of #6.

src/meta.ts
@@ -119,8 +119,8 @@ export const changePropertyType = (data : unknown, to : PropertyType | TypeShape
 
 };
 
-export const choicesAsStrings = (choices? : Choice[]) : string[] => {
-	if (!choices) return [];
+export const choicesAsStrings = (choices? : Choice[]) : string[] | undefined => {
+	if (!choices) return undefined;
 	return choices.map(choice => {
 		if (typeof choice == 'string') return choice;
 		return choice.value;

src/profile.ts
@@ -162,7 +162,7 @@ export class Profile{
 		const def = String(defaultValue);
 		if (!options.choices) return prompt(question, def) || '';
 
-		const strChoices = choicesAsStrings(options.choices);
+		const strChoices = choicesAsStrings(options.choices) || [];
 
 		const finalQuestion = question + '\n\nChoices:\n' + strChoices.join('\n');
 